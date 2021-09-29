14bf7d474cbfa0a0067ca41191c944ed27249363
Mattia Vincenzi <m.vincenzi14@campus.unimib.it>
2021-09-29 15:31:55 +0200

    Start refactor of simulation

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -12,6 +12,13 @@ from behaviour.empirical_strategy import EmpiricalStrategy
 from behaviour.random_strategy import RandomStrategy
 from behaviour.base_strategy import BaseStrategy
 
+data_dir = "../data/"
+nodes = ["node1", "node2", "node3"]
+function_names = ["funca", "qrcode", "ocr"]
+report_output_path = "test/reports/"
+agent_logging_output_base_path = "test/logs/"
+simulation_minutes = 7
+
 # Get a specific logger with passed configurations
 def get_logger(name, log_file, level=logging.INFO):
     handler = logging.FileHandler(log_file)
@@ -21,9 +28,6 @@ def get_logger(name, log_file, level=logging.INFO):
 
     return logger
 
-data_dir = "../data/"
-nodes = ["node1", "node2", "node3"]
-function_names = ["funca", "qrcode", "ocr"]
 
 def gather_configurations():
     node1, node2, node3 = [], [], []
@@ -45,6 +49,7 @@ def gather_configurations():
     return node1, node2, node3
 
 def xfunc_request_table(max_rate_table, invoc_rate_table, weights_table):
+    # Utility prints
     # print("============= MAX RATE TABLE =============")
     # print(max_rate_table)
     # print("==========================================")
@@ -77,27 +82,20 @@ def xfunc_request_table(max_rate_table, invoc_rate_table, weights_table):
                         fwd_requests[node_from][func][node_to] += 1
                     else:
                         fwd_requests[node_from][func][node_to] = 1
-
-            #for node_to, w in weights_x_node.items():
-                # Note: this is correct but when number of agents increase
-                # this is not stable (es: more forwarded requests of received ones)
-            #    fwd_requests[node_from][func][node_to] = int(round((w * \
-            #        (invoc_rate_table[node_from][func] - max_rate_table[node_from][func])) / 100))
-            #fwd_requests[node_from][func][node_from] = invoc_rate_table[node_from][func] # Fixed at line [84-87]
-
-    # print("============= [BEFORE] FORWARDING TABLE TABLE ==============")
-    # print(fwd_requests)
-    # print("============================================================")
  
-    # Fill with remianing values   
+    # Fill forwarding table with missing values
     for node_from, weights_x_func in fwd_requests.items():
         for f in function_names:
             if f not in list(weights_x_func.keys()):
                 fwd_requests[node_from][f] = {}
 
-    nodes_set = set(fwd_requests.keys())
+    nodes_set = set(fwd_requests.keys()) # Set with node keys
 
-    # Fill with remianing values
+    # Fill forwarding table with missing values (missing nodes)
+    # Fill the table diagonal with max_rate if invoc_rate >= max_rate
+    # or with invoc_rate if invoc_rate < max_rate
+    # This means that along the main diagonal there will be the number of request
+    # directly served by the node
     for node_from, weights_x_func in fwd_requests.items():
         for func, weights_x_node in weights_x_func.items():
             for node in nodes_set:
@@ -107,55 +105,32 @@ def xfunc_request_table(max_rate_table, invoc_rate_table, weights_table):
                 fwd_requests[node_from][func][node_from] = invoc_rate_table[node_from][func]
             else:
                 fwd_requests[node_from][func][node_from] = max_rate_table[node_from][func]
-                    
-    # print("============= [AFTER] FORWARDING TABLE TABLE ==============")
+
+    # Utility prints
+    # print("============= FORWARDING TABLE TABLE ==============")
     # print(fwd_requests)
     # print("============================================================")
-    
-    #check_table_validity(fwd_requests, invoc_rate_table, weights_table, max_rate_table)
+
     return fwd_requests
 
-def check_table_validity(table, invoc_rate_table, weights_table, max_rate_table):
-    for node_from, functions in table.items():
-        for func, fwd_requests in functions.items():
-            s = sum(fwd_requests.values())
-            if s == invoc_rate_table[node_from][func]:
-                print("Sum for func {} from {} is equal to {} = invoc rate of {} : CHECK OK".format(
-                    func, node_from, s, invoc_rate_table[node_from][func]))
-            elif s < invoc_rate_table[node_from][func] and max_rate_table[node_from][func] <= invoc_rate_table[node_from][func]:
-                print("Sum for func {} from {} is NOT equal to {} = invoc rate of {} but is OVERLOADED: CHECK OK".format(
-                    func, node_from, s, invoc_rate_table[node_from][func]))
-            else:
-                print("Sum for func {} from {} is NOT equal to {} = invoc rate of {} and UNVERLOADED: CHECK FAILED".format(
-                    func, node_from, s, invoc_rate_table[node_from][func]))
-
-                # Note: this is a simple code that tries to solve the problem of more
-                # requests that not exactly sum to invocation rate. 
-                # Due to the rounding of forwarded requests multiplied for weights 
-                # they do not exactly sum to invocation rate.
-                #
-                # if invoc_rate_table[node_from][func] - s == 1:
-                #     weights = [int(round(w)) for w in weights_table[node_from][func].values()]
-                #     key = np.random.choice(nodes, 1, p=weights)
-                #     table[node_from][func][key] += 1
-                #     s = sum(fwd_requests.values())
-                #     print("     > Sum for func {} from {} is NOT equal to {} = invoc rate of {} : CHECK FIXED".format(
-                #         func, node_from, s, invoc_rate_table[node_from][func]))
-
 def create_tables(fwd_requests, invoc_rate, max_rate, minute, strategy_type):
-    path = "test/reports/" + strategy_type + "/minute_" + str(minute) + "/"
+    path = report_output_path + strategy_type + "/minute_" + str(minute) + "/"
     nodes_set = sorted(set(fwd_requests.keys()))
+
+    # Foreach function and for each node create a dataframe with forwarded requests
     for func in function_names:
         df_x_func = pd.DataFrame([], index=nodes_set)
         for node_from in fwd_requests:
             df_x_func[node_from] = [fwd_requests[node_from][func][k]
                                     for k in sorted(fwd_requests[node_from][func].keys())]
+        # Invert rows and columns
         df_x_func = df_x_func.T
         df_x_func.to_csv(path + func + ".csv", sep='\t', encoding='utf-8')
 
         print("     > FWD_TABLE FOR FUNC {}".format(func))
         print(df_x_func)
-        
+
+    # Create dataframe for invoc_rates
     df_invoc = pd.DataFrame([], index=function_names, columns=nodes_set)
     for node in invoc_rate:
         df_invoc[node] = [invoc_rate[node][f] for f in function_names]
@@ -165,6 +140,7 @@ def create_tables(fwd_requests, invoc_rate, max_rate, minute, strategy_type):
 
     df_invoc.to_csv(path + "invoc_rates.csv", sep='\t', encoding='utf-8')
 
+    # Create dataframe for max_rates
     df_max_rates = pd.DataFrame([], index=function_names, columns=nodes_set)
     for node in max_rate:
         df_max_rates[node] = [max_rate[node][f] for f in function_names]
@@ -174,11 +150,18 @@ def create_tables(fwd_requests, invoc_rate, max_rate, minute, strategy_type):
     print("     > MAX_RATE_TABLE")
     print(df_max_rates)
 
+"""
+    Core function. 
+    This function allow to create random cluster situation, giving them as input (JSON
+    files) to agents (with differents behaviours) that calculate with different strategies
+    the forwarding weights for OVERLOADED functions
+"""
 def simulation(nodes_number, node1_config, node2_config, node3_config):
     # 1) For each node pick a random file
     configurations = []
+
     # Not used a uniform distribution because is more likely to have a
-    # high number of node1 nodes, with minor specs due to gerarchic
+    # high number of node of type 1, with minor specs due to gerarchic
     # cloud infrastructure
     choiches = np.random.choice(nodes, nodes_number, p=[0.5, 0.3, 0.2])
     for choice in choiches:
@@ -192,9 +175,9 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
     #print(configurations)
 
     # Mock loaded files [TO BE REMOVED]
-    # configurations = ["exp-comparison/case2/node1.json",
-    #                   "exp-comparison/case2/node2.json", 
-    #                   "exp-comparison/case2/node3.json"]
+    # configurations = ["exp-comparison/case3/node1.json",
+    #                   "exp-comparison/case3/node2.json", 
+    #                   "exp-comparison/case3/node3.json"]
     # print(configurations)
 
     # 2) Load selected files
@@ -217,7 +200,7 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
         execution_times_rs[id] = []
         execution_times_es[id] = []
 
-    for minute in range(0, 7): # 6 minutes
+    for minute in range(0, simulation_minutes): # 6 minutes
         final_config = {}
         
         # Dictionaries used for analysis
@@ -233,6 +216,7 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
             f['output'][minute]['node'] = f['input']['node']
             final_config[key] = f['output'][minute]
 
+            # Create and fill invoc_rate and max_rate dictionaries with loaded values
             simulation_invoc_rate_table[key] = {}
             simulation_max_rate_table[key] = {}
             for func in final_config[key]["functions"]:
@@ -242,23 +226,24 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
             
         #print(final_config)
         
-        # Fill max_rate and invoc_rate table
+        # Fill max_rate and invoc_rate table with missing values
         for node, weights_x_func in simulation_invoc_rate_table.items():
             for f in function_names:
                 if f not in list(weights_x_func.keys()):
                     simulation_invoc_rate_table[node][f] = 0
         
-        # Fill max_rate and invoc_rate table
+        # Fill max_rate and invoc_rate table with missing values
         for node, weights_x_func in simulation_max_rate_table.items():
             for f in function_names:
                 if f not in list(weights_x_func.keys()):
                     simulation_max_rate_table[node][f] = 0
 
-        # Write configuration on file
+        # Write configuration on file for logging
         with open('test/final_config/config{}.json'.format(minute), 'w', encoding='utf-8') as f:
             json.dump(final_config, f, ensure_ascii=False, indent=4)
 
         # 4) Call agent loop for each config that has been previously built
+        #
         # With last update this code is executed for each type of behaviour
         # (base, random and empirical) and for each agent in the network
         for id in range(0, nodes_number):
@@ -268,7 +253,7 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
                 id,
                 get_logger(
                     "agent" + str(id) + "_minute_" + str(minute) + "_bs",
-                    "test/logs/agent" + str(id) + "_minute_" + str(minute) + "_bs.log"
+                    agent_logging_output_base_path + "agent" + str(id) + "_minute_" + str(minute) + "_bs.log"
                 ),
                 BaseStrategy("", False, final_config)
             )
@@ -284,7 +269,7 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
                 id,
                 get_logger(
                     "agent" + str(id) + "_minute_" + str(minute) + "_rs",
-                    "test/logs/agent" + str(id) + "_minute_" + str(minute) + "_rs.log"
+                    agent_logging_output_base_path + "agent" + str(id) + "_minute_" + str(minute) + "_rs.log"
                 ),
                 RandomStrategy("", False, final_config)
             )
@@ -300,7 +285,7 @@ def simulation(nodes_number, node1_config, node2_config, node3_config):
                 id,
                 get_logger(
                     "agent" + str(id) + "_minute_" + str(minute) + "_es",
-                    "test/logs/agent" +
+                    agent_logging_output_base_path + "agent" +
                     str(id) + "_minute_" + str(minute) + "_es.log"
                 ),
                 EmpiricalStrategy("", False, final_config)
@@ -364,43 +349,5 @@ def run_agent(agent):
     
     return weights, execution
 
-"""
-    TODO: Not still working
-"""
-def simulation_with_graphs():
-    exp_times = []
-    for i in range(5, 105, 5):
-        repaeted_simulation_times = []
-        for _ in range(0, 20):
-            sim_time = simulation(i, node1_config, node2_config, node3_config)
-            repaeted_simulation_times.append(sim_time)
-        mean = np.mean(repaeted_simulation_times)
-        exp_times.append(mean)
-        print("Mean execution time over 10 repeated experiments with {} nodes is: {}".format(i, mean))
-
-    print(exp_times)
-
-    df = pd.DataFrame()
-
-    df["experiment"] = range(5, 105, 5)
-    df["time"] = exp_times
-
-    # Plot configurations
-    plt.figure(figsize=(20, 10))
-    plt.title(
-        "Agent execution time in function of the p2p net size or the number of neighbours")
-    plt.xlabel("Experiment file")
-    plt.ylabel("MAPE loop -- execution time (seconds)")
-
-    plt.plot(df["experiment"], df["time"],
-            label="Experiment with differents agents num")
-
-    # Plot configurations
-    plt.legend(loc="upper left")
-    plt.grid()
-
-    plt.savefig("comparison.png")
-
 node1_config, node2_config, node3_config = gather_configurations()
-#simulation_with_graphs()
 simulation(3, node1_config, node2_config, node3_config)
