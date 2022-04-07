bcf568142cbb3ce8c9b915d7a786ae9c288a0c00
Alessandro Tundo <alessandrotundo94@gmail.com>
2022-04-07 15:49:03 +0200

    Add echos to deploy script

--- utils/deploy-fuctions-to-nodes.sh
+++ utils/deploy-fuctions-to-nodes.sh
@@ -8,10 +8,12 @@ NODE_SUFFIX=$3
 
 for ((i=1;i<=NODES;i++)); do
   CONTAINER_NAME="$NODE_PREFIX$i$NODE_SUFFIX"
+  echo "Deploying functions to $CONTAINER_NAME..."
   docker exec -it ${CONTAINER_NAME} ./deploy_functions.sh # it may requires few minutes due to functions images download
 done
 
 for ((i=1;i<=NODES;i++)); do
   CONTAINER_NAME="$NODE_PREFIX$i$NODE_SUFFIX"
+  echo "Restarting $CONTAINER_NAME node..."
   docker exec -it ${CONTAINER_NAME} "systemctl restart dfaasagent"
 done
