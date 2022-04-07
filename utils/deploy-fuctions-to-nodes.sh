a186bab79c4e67a17b31936f37018a68a32e9b54
Alessandro Tundo <alessandrotundo94@gmail.com>
2022-04-07 15:47:04 +0200

    Fix deploy script

--- utils/deploy-fuctions-to-nodes.sh
+++ utils/deploy-fuctions-to-nodes.sh
@@ -6,12 +6,12 @@ NODES=$1
 NODE_PREFIX=$2
 NODE_SUFFIX=$3
 
-for i in {1..NODES} ; do
+for ((i=1;i<=NODES;i++)); do
   CONTAINER_NAME="$NODE_PREFIX$i$NODE_SUFFIX"
   docker exec -it ${CONTAINER_NAME} ./deploy_functions.sh # it may requires few minutes due to functions images download
 done
 
-for i in {1..$NODES} ; do
-  CONTAINER_NAME="dfaas-node-$i-1"
+for ((i=1;i<=NODES;i++)); do
+  CONTAINER_NAME="$NODE_PREFIX$i$NODE_SUFFIX"
   docker exec -it ${CONTAINER_NAME} "systemctl restart dfaasagent"
 done
