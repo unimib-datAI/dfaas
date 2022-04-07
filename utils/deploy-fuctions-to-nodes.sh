fe0a8aca49f488d0d758e44aafc89cbdfc79e9b6
Alessandro Tundo <alessandrotundo94@gmail.com>
2022-04-07 15:44:48 +0200

    Fix deploy script

--- utils/deploy-fuctions-to-nodes.sh
+++ utils/deploy-fuctions-to-nodes.sh
@@ -6,7 +6,7 @@ NODES=$1
 NODE_PREFIX=$2
 NODE_SUFFIX=$3
 
-for i in {1..$NODES} ; do
+for i in {1..NODES} ; do
   CONTAINER_NAME="$NODE_PREFIX$i$NODE_SUFFIX"
   docker exec -it ${CONTAINER_NAME} ./deploy_functions.sh # it may requires few minutes due to functions images download
 done
