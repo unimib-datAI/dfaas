fd8dbac02a7c6e458e0eddc708e36ea7fb56cad1
Alessandro Tundo <alessandrotundo94@gmail.com>
2022-04-22 01:28:33 +0200

    Fix install.sh for emulator

--- emulator/install.sh
+++ emulator/install.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get install -yy ansible git
 
 # git clone --branch v3.1 https://github.com/containernet/containernet.git
-git clone https://github.com/ElectricalBoy/containernet.gitt
+git clone https://github.com/ElectricalBoy/containernet.git
 
 #  We opened a pull request (#243) to make this edit available directly from upstream. See the PR for further details.
 cp hack/node.py containernet/mininet/node.py
