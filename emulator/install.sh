5bea6b8801da1eb560f15814e507ab425999523c
Alessandro Tundo <alessandrotundo94@gmail.com>
2022-04-22 01:03:00 +0200

    Fix install.sh for emulator

--- emulator/install.sh
+++ emulator/install.sh
@@ -8,7 +8,8 @@ git clone --branch v3.1 https://github.com/containernet/containernet.git
 
 #  We opened a pull request (#243) to make this edit available directly from upstream. See the PR for further details.
 cp hack/node.py containernet/mininet/node.py
+cp hack/install.yml containernet/ansible/install.yml
 
-sudo ansible-playbook -i "localhost," -c local hack/install.yml
+sudo ansible-playbook -i "localhost," -c local containernet/ansible/install.yml
 
 sudo pip3 install -r requirements.txt
