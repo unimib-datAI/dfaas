e03441886e51c2c43096943321805ddab5e33f88
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2025-09-04 13:30:06 +0000

    remove unused files from old deploy method

--- docker/files/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Generate dfaasagent.env file with all the env vars prefixed with `AGENT_`.
-# This file is used by the systemd unit dfaasagent.service.
-env | grep ^AGENT_ > /agent/dfaasagent.env
-exec /sbin/init --log-level=err
