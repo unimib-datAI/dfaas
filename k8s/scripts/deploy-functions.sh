0de448b2a5382a2834b9680425cb5a4f27c2cc19
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2025-09-24 13:13:36 +0000

    k8s: allow to choose which function to deploy

--- k8s/scripts/deploy-functions.sh
+++ k8s/scripts/deploy-functions.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
-#
-# Small utility to deploy three OpenFaaS functions to the local k3s cluster.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright 2025 The DFaaS Authors. All rights reserved.
+# This file is licensed under the AGPL v3.0 or later license. See LICENSE and
+# AUTHORS file for more information.
 set -euo pipefail
+
+# Show help message if requested.
+if [[ $# -gt 0 && ( "$1" == "-h" || "$1" == "--help" ) ]]; then
+    echo "Usage: $0 [FUNCTION ...]"
+    echo
+    echo "Deploy OpenFaaS functions to the local k3s cluster."
+    echo
+    echo "Options:"
+    echo "  -h, --help    Show this help message and exit."
+    echo
+    echo "Arguments:"
+    echo "  FUNCTION      List of functions to deploy. If none are provided,"
+    echo "                defaults (figlet, shasum, ocr) will be used."
+    exit 0
+fi
+
+# Get function from CLI or defaults.
+if [[ $# -eq 0 ]]; then
+    FUNCTIONS=(figlet shasum ocr)
+else
+    FUNCTIONS=("$@")
+fi
 
 # Read credentials from the 'basic-auth' Kubernetes secret.
 PASSWORD=$(sudo kubectl get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode)
@@ -12,7 +36,6 @@ faas-cli login --password "$PASSWORD"
 echo
 
 # Iterate over the default functions.
-FUNCTIONS=(figlet shasum ocr)
 for func in "${FUNCTIONS[@]}"; do
     # Check if it is already deployed.
     if [[ $(faas-cli list --quiet | grep -x "$func") ]]; then
