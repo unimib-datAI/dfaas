20f6a66911a3be3f2c1023eb254b1c6fc7261aa9
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2025-12-19 17:56:34 +0100

    Fix Minikube node builder script

--- metrics_predictions/minikube_builder.sh
+++ metrics_predictions/minikube_builder.sh
@@ -1,7 +1,6 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-# check if minikube is running, delete the instance if it is running
-
+echo "Checking if Minikube is running..."
 STATUS=$(minikube status --format='{{.Host}}')
 if [ "$STATUS" = "Running" ]; then
   echo A Minikube instance is running.
@@ -17,11 +16,9 @@ else
   echo Minikube is not running
 fi
 
-# build the minikube instance
-
 echo Building Minikube instance...
 
-minikube start --memory=max --cpus=max  --apiserver-ips=10.99.217.210
+minikube start --memory=max --cpus=max  --apiserver-ips=10.12.38.4
 
 if [ $? -eq 0 ]; then
   echo Minikube is up and running
@@ -31,31 +28,32 @@ else
 fi
 
 # Loading pre pulled docker images
+#
+# TODO: Skip this for now.
 
 # Folder containing the .tar files from pull-and-save-images.sh
-IMAGE_DIR=~/openfaas-image-cache
+#IMAGE_DIR=~/openfaas-image-cache
 
 # Check the directory exists
-if [ ! -d "$IMAGE_DIR" ]; then
-  echo " Error: $IMAGE_DIR does not exist. Run the pull script first."
-  exit 1
-fi
+#if [ ! -d "$IMAGE_DIR" ]; then
+#  echo " Error: $IMAGE_DIR does not exist. Run the pull script first."
+#  exit 1
+#fi
 
-echo "[*] Loading saved images into Minikube from $IMAGE_DIR..."
+#echo "[*] Loading saved images into Minikube from $IMAGE_DIR..."
 
-for TAR in "$IMAGE_DIR"/*.tar; do
-  echo "-> Loading $TAR into Minikube"
-  minikube image load "$TAR"
-done
+#for TAR in "$IMAGE_DIR"/*.tar; do
+#  echo "-> Loading $TAR into Minikube"
+#  minikube image load "$TAR"
+#done
 
-echo "All .tar images successfully loaded into Minikube"
-
-# Installing openfaas on minikube
+#echo "All .tar images successfully loaded into Minikube"
 
 echo Installing openfaas on the Minikube instance...
-arkade install openfaas
+arkade install openfaas-ce
 if [ $? -eq 0 ]; then
   echo Setting up openfaas...
+  # From official OpenFaaS docs
   kubectl rollout status -n openfaas deploy/gateway
   kubectl port-forward -n openfaas svc/gateway 8080:8080 >/dev/null 2>&1 &
 
@@ -82,21 +80,18 @@ fi
 
 # Start building the cluster using the infrastructure dir
 
- echo Building node infrastructure...
- kubectl apply -f ~/dfaas/metrics_predictions/infrastructure
-
- if [ $? -eq 0 ]; then
-   echo Node has been built succesfully!
- else
-   echo Unable to build the node
-   exit 1
- fi
+echo Building node infrastructure...
+kubectl apply -f ~/dfaas/metrics_predictions/infrastructure
+if [ $? -eq 0 ]; then
+  echo Node has been built succesfully!
+else
+  echo Unable to build the node
+  exit 1
+fi
 
 sleep 20
 
-# Restart prometheus pod
-
-echo Restating Promtheus pod
+echo "Restating Promtheus pod..."
 POD=$(kubectl get pods -n openfaas -l app=prometheus -o jsonpath="{.items[0].metadata.name}")
 kubectl delete pod $POD -n openfaas
 if [ $? -eq 0 ]; then
@@ -106,9 +101,7 @@ else
   exit 1
 fi
 
-# Copy the the find-pid script into the minikube container
-
-echo Copying find-pid.py script into the Minikube container
+echo "Copying find-pid.py script into the Minikube container..."
 docker cp ~/dfaas/metrics_predictions/samples_generator/find-pid.py minikube:/etc/
 
 if [ $? -eq 0 ]; then
