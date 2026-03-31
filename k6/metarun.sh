d5e190aefd3375d0c07c16a431d4b0400725e4f1
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2026-03-31 15:33:50 +0000

    Update k6/metarun.sh

--- k6/metarun.sh
+++ k6/metarun.sh
@@ -6,14 +6,34 @@ set -euo pipefail
 # Make sure the working directory is where this script is located!
 cd "$(dirname "$0")"
 
-EXP_NAME=auto_scaled TRACE_PATH=input_requests_scaled_traces.json ./run.sh
+EXP_NAME=input_trace_sinusoidal_78_mlimage_scaled TRACE_PATH=input_trace_sinusoidal_78_mlimage_scaled.json ./run.sh
 echo
+echo
+echo "Waiting 5 minutes..."
+sleep 300
+echo "Waiting done!"
 echo
 
-EXP_NAME=auto_scaled_power_2 TRACE_PATH=input_requests_scaled_power_2_traces.json ./run.sh
+EXP_NAME=input_trace_sinusoidal_78_mlimage_scaled_pwr_75 TRACE_PATH=input_trace_sinusoidal_78_mlimage_scaled_pwr_75.json ./run.sh
 echo
+echo
+echo "Waiting 5 minutes..."
+sleep 300
+echo "Waiting done!"
 echo
 
-EXP_NAME=auto_scaled_power_1 TRACE_PATH=input_requests_scaled_power_1_traces.json ./run.sh
+EXP_NAME=input_trace_sinusoidal_78_mlimage_scaled_pwr_50 TRACE_PATH=input_trace_sinusoidal_78_mlimage_scaled_pwr_50.json ./run.sh
 echo
+echo
+echo "Waiting 5 minutes..."
+sleep 300
+echo "Waiting done!"
+echo
+
+EXP_NAME=input_trace_sinusoidal_78_mlimage_scaled_pwr_25 TRACE_PATH=input_trace_sinusoidal_78_mlimage_scaled_pwr_25.json ./run.sh
+echo
+echo
+echo "Waiting 5 minutes..."
+sleep 300
+echo "Waiting done!"
 echo
