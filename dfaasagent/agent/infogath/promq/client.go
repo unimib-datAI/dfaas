fcaba49ddb4f24d3081811865aee422211ccde78
Emanuele Petriglia <inbox@emanuelepetriglia.com>
2026-04-08 15:26:42 +0200

    Add CPUUsage Prometheus query in agent

--- dfaasagent/agent/infogath/promq/client.go
+++ dfaasagent/agent/infogath/promq/client.go
@@ -97,3 +97,72 @@ func (c *Client) Replicas(start, end time.Time) (map[string]uint, error) {
 
 	return replicas, nil
 }
+
+// CPUUsage returns the average CPU usage (normalized in [0, 1]) per container
+// between start and end. Missing containers are set to 0 and warned.
+func (c *Client) CPUUsage(start, end time.Time, containers []string) (map[string]float32, error) {
+	if end.Before(start) {
+		return nil, errors.New("end time must be after start time")
+	}
+	if len(containers) == 0 {
+		return nil, errors.New("containers list cannot be empty")
+	}
+
+	// Same as Replicas().
+	duration := end.Sub(start)
+
+	// Query mainly taken from k8s/scripts/prometheus2csv/metrics.csv
+	containerRegex := strings.Join(containers, "|")
+	query := fmt.Sprintf(`
+avg by (container) (
+  avg_over_time(
+    (
+      irate(container_cpu_usage_seconds_total{
+        namespace="default",
+        container=~"%s",
+        container!=""
+      }[1m])
+      / on(instance) group_left machine_cpu_cores
+    )[%s]
+  )
+)`, containerRegex, duration.String())
+
+	ctx := context.Background()
+	result, warnings, err := c.promAPI.Query(ctx, query, end)
+	if err != nil {
+		return nil, fmt.Errorf("get CPU usage from Prometheus: %w", err)
+	}
+	if len(warnings) > 0 {
+		c.logger.Warnf("Prometheus warnings for query %q\n%s", query, strings.Join(warnings, "\n"))
+	}
+
+	// See Replicas().
+	vector, ok := result.(model.Vector)
+	if !ok {
+		return nil, fmt.Errorf("result type is %T, expected %T", model.Vector{})
+	}
+
+	// Initialize return value.
+	usage := make(map[string]float32, len(containers))
+	found := make(map[string]bool, len(containers))
+	for _, c := range containers {
+		usage[c] = 0
+		found[c] = false
+	}
+
+	for _, sample := range vector {
+		// Note in this case there is no namespace in container name.
+		container := string(sample.Metric["container"])
+		usage[container] = float32(sample.Value)
+		found[container] = true
+	}
+
+	// Warn for missing containers.
+	for _, cont := range containers {
+		if !found[cont] {
+			c.logger.Warnf("no CPU data for container %q in given time range", cont)
+		}
+	}
+
+	return usage, nil
+}
